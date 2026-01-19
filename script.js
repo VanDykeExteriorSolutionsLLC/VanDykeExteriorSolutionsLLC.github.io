580d323412976c71fc9f3434b38c1608422e8fff
VanDykeExteriorSolutionsLLC <vandykeexteriorsolutionsllc@gmail.com>
2026-01-18 19:44:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function toggleMenu() {
-  document.getElementById("navLinks").classList.toggle("show");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  alert("JS LOADED");
+
+  window.toggleMenu = function () {
+    document.getElementById("navLinks").classList.toggle("show");
+  };
+});

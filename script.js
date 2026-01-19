a160837a80ae2ae132ddc02387307e24e5300fd1
VanDykeExteriorSolutionsLLC <vandykeexteriorsolutionsllc@gmail.com>
2026-01-18 19:47:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  alert("JS LOADED");
-
   window.toggleMenu = function () {
     document.getElementById("navLinks").classList.toggle("show");
   };
